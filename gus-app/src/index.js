5d6038dd281ad1b935970fb0822762b5839eb314
gferrari18 <gferrari18@my.whitworth.edu>
2022-07-26 22:58:57 -0700

    Calc hw

--- gus-app/src/index.js
+++ gus-app/src/index.js
@@ -2,15 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+class Mariners extends React.Component{
+    render() {
+        return (
+            <img src="https://c1.neweggimages.com/WebResource/Themes/Nest/logos/Newegg_full_color_logo_RGB.SVG" alt="nice"></img>
+        )
+    }
+}
+
 class Main extends React.Component{
+    constructor() {
+        super()
+
+        this.state = {
+            'count': 0
+        }
+    }
+
+    incrementCount() {
+        const { count } = this.state
+        this.setState({...this.state, count: count+1})
+    }
+
+    decrementCount() {
+        const { count } = this.state
+        this.setState({...this.state, count: count-1})
+    }
+
     render(){
+        const {user} = this.props
+        const {count} = this.state
         return (
             <div className='Main'>
-                <p>Hello World</p>
+                <p>Hello {user}</p>
+                <Mariners/>
+                <p>{count} clicks</p>
+                <button onClick={this.incrementCount.bind(this)}>increment</button>
+                <button onClick={this.decrementCount.bind(this)}>Decrement</button>
             </div>
         )
     }
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Main/>);
+root.render(<Main user='Gus' />);
